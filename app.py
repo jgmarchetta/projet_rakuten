757e5eff93e6751006dcfbec2007190b50f20446
Jean-Gabriel <164254516+jgmarchetta@users.noreply.github.com>
2024-07-01 15:01:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-from bs4 import BeautifulSoup
+import streamlit as st
 import pandas as pd
+import gdown
 
-# Lire le fichier HTML
-with open('/mnt/data/fichier.html', 'r', encoding='utf-8') as file:
-    soup = BeautifulSoup(file, 'html.parser')
+def download_file_from_google_drive(drive_url, output_path):
+    gdown.download(drive_url, output_path, quiet=False)
 
-# Trouver toutes les tables dans le fichier HTML
-tables = soup.find_all('table')
+# URL du fichier Google Drive (lien de partage)
+drive_url = 'https://drive.google.com/uc?id=1dI7zqHcU1XhafXHNyk_339Oh6iPvqAzO'
 
-# Si des tables existent, extraire les données et les convertir en CSV
-if tables:
-    for index, table in enumerate(tables):
-        # Lire la table en utilisant pandas
-        df = pd.read_html(str(table))[0]
-        
-        # Sauvegarder la table dans un fichier CSV
-        csv_filename = f'/mnt/data/table_{index + 1}.csv'
-        df.to_csv(csv_filename, index=False)
-        print(f'Table {index + 1} sauvegardée sous {csv_filename}')
-else:
-    print("Aucune table trouvée dans le fichier HTML.")
+# Chemin de sortie pour le fichier téléchargé
+output_path = 'X_train_update.csv'
+
+# Télécharger le fichier
+download_file_from_google_drive(drive_url, output_path)
+
+# Lire le fichier CSV
+df = pd.read_csv(output_path)
+
+# Afficher le dataframe dans Streamlit
+st.write("Aperçu du fichier CSV téléchargé :")
+st.dataframe(df)
